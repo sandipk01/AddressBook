5f96ad0a46f60c4d2d63777e1fcf950caf183b1c
sandipk01 <sandipkengar01@gmail.com>
2020-05-02 00:44:43 +0530

    ADDED : Sort by last name method is declared

--- src/main/java/com/bridgelabz/addressbook/service/IAddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/service/IAddressBook.java
@@ -7,9 +7,16 @@ import java.io.IOException;
 import java.util.List;
 
 public interface IAddressBook {
+
     List<Person> addPerson(Person person) throws IOException;
+
     List<Person> editPerson(Person person, String phoneNumber) throws IOException, AddressBookException;
+
+    List<Person> deletePerson(String phoneNumber) throws IOException, AddressBookException;
+
     IFile getFileSystem();
+
     boolean isPersonAdded(List<Person> personList, Person person);
 
+    List<Person> sortByLastName() throws IOException;
 }
