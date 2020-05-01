f2acd4e9cb80a1fad97104521e83ceec40066887
sandipk01 <sandipkengar01@gmail.com>
2020-05-01 09:37:33 +0530

    ADDED : When given file created then should return true

--- src/test/java/com/bridgelabz/test/AddressBookTest.java
+++ src/test/java/com/bridgelabz/test/AddressBookTest.java
@@ -1,26 +1,18 @@
 package com.bridgelabz.test;
 
+import com.bridgelabz.addressbook.exception.AddressBookException;
 import com.bridgelabz.addressbook.service.FileSystem;
-import com.bridgelabz.addressbook.service.IFile;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+
 
 public class AddressBookTest {
 
     @Test
-    public void givenFile_WhenAddedInDirectory_ShouldReturnExactCount() throws IOException {
-        long count;
-        FileSystem fileSystem = new FileSystem("file1");
-        FileSystem fileSysem = new FileSystem("file2");
-        try (Stream<Path> files = Files.list(Paths.get(IFile.FILEPATH))) {
-            count = files.count();
-        }
-        Assert.assertEquals(2, count);
+    public void givenJsonFile_WhenAddedInDirectory_ShouldReturnTrue() throws IOException, AddressBookException {
+        FileSystem fileSystem = new FileSystem();
+        Assert.assertEquals(true, fileSystem.createFile("file1"));
     }
 }
