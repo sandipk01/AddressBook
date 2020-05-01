d2f0793a2446bfe212afaca0e0344d9954242d5d
sandipk01 <sandipkengar01@gmail.com>
2020-05-01 09:36:43 +0530

    ADDED : Create file method is added

--- src/main/java/com/bridgelabz/addressbook/service/IFile.java
+++ src/main/java/com/bridgelabz/addressbook/service/IFile.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook.service;
 
+import com.bridgelabz.addressbook.exception.AddressBookException;
 import com.bridgelabz.addressbook.model.Person;
 
 import java.io.IOException;
@@ -8,6 +9,8 @@ import java.util.List;
 public interface IFile {
 
     String FILEPATH = "src\\test\\resources\\";
+
+    boolean createFile(String file) throws IOException, AddressBookException;
 
     void saveFile(List<Person> personList) throws IOException;
 
