e7662e464a861be6c5b26a28be5d2818780e95fb
sandipk01 <sandipkengar01@gmail.com>
2020-05-01 16:59:33 +0530

    ADDED : Declared methods

--- src/main/java/com/bridgelabz/addressbook/service/IAddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/service/IAddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook.service;
 
+import com.bridgelabz.addressbook.exception.AddressBookException;
 import com.bridgelabz.addressbook.model.Person;
 
 import java.io.IOException;
@@ -7,5 +8,8 @@ import java.util.List;
 
 public interface IAddressBook {
     List<Person> addPerson(Person person) throws IOException;
-    List<Person> editPerson(Person person, String phoneNumber) throws IOException;
+    List<Person> editPerson(Person person, String phoneNumber) throws IOException, AddressBookException;
+    IFile getFileSystem();
+    boolean isPersonAdded(List<Person> personList, Person person);
+
 }
