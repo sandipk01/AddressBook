c2ffd3bd9dc020e88451149e4057c7861640ea86
sandipk01 <sandipkengar01@gmail.com>
2020-05-01 11:22:38 +0530

    ADDED : Method declared addPerson

--- src/main/java/com/bridgelabz/addressbook/service/IAddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/service/IAddressBook.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.addressbook.service;
 
+import com.bridgelabz.addressbook.model.Person;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface IAddressBook {
+    List<Person> addPerson(Person person) throws IOException;
 }
