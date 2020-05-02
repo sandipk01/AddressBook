a136354eb67146f773b3600bc93bb4954a037170
sandipk01 <sandipkengar01@gmail.com>
2020-05-02 11:18:18 +0530

    ADDED : Address book main application is added

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook;
+
+import com.bridgelabz.addressbook.controller.AddressBookController;
+import com.bridgelabz.addressbook.exception.AddressBookException;
+
+import java.io.IOException;
+
+public class AddressBookApplication {
+    public static void main(String[] args) throws IOException, AddressBookException {
+        AddressBookController addressBookController=new AddressBookController();
+        addressBookController.dashBoard();
+    }
+}
---
+++ src/main/java/com/bridgelabz/addressbook/controller/AddressBookController.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.addressbook.controller;
+
+public class AddressBookController {
+}
